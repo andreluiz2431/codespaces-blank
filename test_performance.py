f7dad856a2e49c6eb4299946fc6da3774865a48a
André Luiz <42250150+andreluiz2431@users.noreply.github.com>
2024-11-19 19:08:15 +0000

    ajustando boas praticas

--- test_performance.py
+++ test_performance.py
@@ -16,11 +16,11 @@ def receive_tcp_response(socket):
 def create_udp_socket():
     return socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-def send_udp_request(client_socket, request, address):
-    client_socket.sendto(request.encode(), address)
+def send_udp_request(socket, request, address):
+    socket.sendto(request.encode(), address)
 
-def receive_udp_response(client_socket):
-    response, _ = client_socket.recvfrom(4096)
+def receive_udp_response(socket):
+    response, _ = socket.recvfrom(4096)
     return response
 
 def test_tcp_performance():
@@ -34,20 +34,20 @@ def test_tcp_performance():
             request = "GET / HTTP/1.1\nHost: localhost\n\n"
             send_tcp_request(client_socket, request)
             response = receive_tcp_response(client_socket)
-            client_socket.close()
         except Exception as e:
             print(f"Erro na conexão ou envio: {e}")
     
     end_time = time.time()
+    client_socket.close()
     print(f"TCP: Time taken for 10,000 requests: {end_time - start_time:.2f} seconds")
 
 def test_udp_performance():
+    server_address = ('server_udp', 8080)
     start_time = time.time()
 
     for _ in range(10000):
         try:
             client_socket = create_udp_socket()
-            server_address = ('server_udp', 8080)
             request = "GET / HTTP/1.1\nHost: localhost\n\n"
             send_udp_request(client_socket, request, server_address)
             response = receive_udp_response(client_socket)
